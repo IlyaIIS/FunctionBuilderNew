f80605f5c09ce2467de4fc9b3a188696463f27a9
IlyaIIS <ilya1221221@mail.ru>
2021-02-23 19:05:47 +0500

    V1.3 Добавлены тесты

---
+++ FunctionBuilder.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace FunctionBuilder
+{
+    public class Tests
+    {
+        //ввод верных данных в опз
+        //ввод неверных данных в опз
+        //ввод неверных данных в опз
+        //ввод верного опз в калькулятор
+        //ввод неверного опз в калькулятор
+
+        [TestCase]
+        public void CalculatorTest()
+        {
+            var expression = "round(5*1.3)^2";
+            Assert.AreEqual(OPZ.ParseExpression(expression), new List<string> {"5", "1.3", "*", "round", "2", "^" });
+        }
+
+        [TestCase("sinx", ExpectedResult = false)]
+        [TestCase("((x+1)*2)", ExpectedResult = true)]
+        public bool FormulaCorrectlyTest(string formula)
+        {
+            return OPZ.IsFormulaCorrectly(formula, out string s);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void CalculatorTests(List<string> expression, double result)
+        {
+            Assert.AreEqual(result, OPZ.Calculate(expression));
+        }
+        public static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new List<string> { "1", "2", "+" }, 3);
+                yield return new TestCaseData(new List<string> { "3", "1", "2", "+", "*" }, 9);
+            }
+        }
+
+    }
+}
